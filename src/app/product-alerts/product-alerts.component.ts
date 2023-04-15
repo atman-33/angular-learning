1260bc369965924b57eb2878cf728d30a10ced55
atman-33 <41929192+atman-33@users.noreply.github.com>
2023-04-15 16:38:22 +0900

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent {
 
+  // 別コンポーネントからデータを受け取る際は、@Input() を付与
+  @Input() name!: string;
+
+  constructor() {
+  }
+
+  /**
+   * コンストラクタの次に呼ばれる関数
+   */
+  ngOnInit() {}
 }
